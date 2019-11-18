46a2c85a71575ae4af8843cbe9c3b2a753f4b5ad
Doug Coulter <dcfusor@gmail.com>
2019-11-18 14:36:29 -0500

    Add files via upload

---
+++ InstallDependencies/Gtk3Install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt-get install cpanminus build-essential gobject-introspection libcairo-gobject-perl libglib-object-introspection-perl libdevice-serialport-perl libgirepository1.0-dev libgtk3-perl
+sudo cpanm Modern::Perl
+#sudo cpanm --force Gtk3
+
